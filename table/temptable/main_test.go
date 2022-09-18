7f37f6beb0bf259bc21ec1c3075ce70af6d22b36
Weizhen Wang <wangweizhen@pingcap.com>
2022-09-19 00:39:20 +0800

    *: goleak remove go.opencensus.io
    Signed-off-by: Weizhen Wang <wangweizhen@pingcap.com>

--- table/temptable/main_test.go
+++ table/temptable/main_test.go
@@ -36,7 +36,6 @@ import (
 func TestMain(m *testing.M) {
 	opts := []goleak.Option{
 		goleak.IgnoreTopFunction("go.etcd.io/etcd/client/pkg/v3/logutil.(*MergeLogger).outputLoop"),
-		goleak.IgnoreTopFunction("go.opencensus.io/stats/view.(*worker).start"),
 		goleak.IgnoreTopFunction("github.com/golang/glog.(*loggingT).flushDaemon"),
 	}
 	testsetup.SetupForCommonTest()
