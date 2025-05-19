0339b9c22e4e8f774effdc6e97da125546c60d83
Weizhen Wang <wangweizhen@pingcap.com>
2025-05-19 20:19:22 +0800

    fix wrong cost
    Signed-off-by: Weizhen Wang <wangweizhen@pingcap.com>

--- pkg/planner/core/tiflash_selection_late_materialization.go
+++ pkg/planner/core/tiflash_selection_late_materialization.go
@@ -278,7 +278,7 @@ func predicatePushDownToTableScanImpl(sctx base.PlanContext, physicalSelection *
 
 // PushedDown is used to push down the selected conditions from PhysicalSelection to PhysicalTableScan.
 // Used in unit test, so it is exported.
-func PushedDown(sel *PhysicalSelection, ts *PhysicalTableScan, selectedConds []expression.Expression, selectedSelectivity float64) {
+func PushedDown(sel *PhysicalSelection, ts *PhysicalTableScan, selectedConds []expression.Expression, _ float64) {
 	// remove the pushed down conditions from selection
 	removeSpecificExprsFromSelection(sel, selectedConds)
 	// add the pushed down conditions to table scan
